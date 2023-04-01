080fdc4fa5ba60430887933c47e0ea80669e841e
Vinicius-g-r <viniciussouza26032005@gmail.com>
2023-03-31 22:00:18 -0300

    TERMINEIII

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string especie, raça, alcunha, idade, pelagemcor;
+
+Console.WriteLine("Qual é a espécie do seu Pet? ");
+especie = Console.ReadLine()!;
+
+Console.WriteLine("Qual é a raça do seu Pet? ");
+raça = Console.ReadLine()!;
+
+Console.WriteLine("Qual é o nome do seu Pet? ");
+alcunha = Console.ReadLine()!;
+
+Console.WriteLine("Qual é a idade do seu Pet? ");
+idade = Console.ReadLine()!;
+
+Console.WriteLine("Qual é a pelagem do seu Pet? ");
+pelagemcor = Console.ReadLine()!;
+
+Console.WriteLine("+=========================================================+");
+Console.WriteLine("|                 Pet Hotel Beach Pet                     |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine($"| Espécie: {especie.PadLeft(17,'.')}  | Raça: {raça.PadLeft(19,'.')} |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine($"| Atende pela alcunha de: {alcunha.PadLeft(31,'.')} |");
+Console.WriteLine($"| Idade: {idade.PadLeft(12,'.')} anos(s) | Pelagem/cor: {pelagemcor.PadLeft(12,'.')} |");
+Console.WriteLine("+=========================================================+");
